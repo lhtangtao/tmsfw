f4e5167f936b35e41b70ad76c6e7c4acdc9120f4
tao tang <rache008@163.com>
2018-05-31 01:22:10 +0800

    新增加了一个函数

--- tttttt.py
+++ tttttt.py
@@ -14,35 +14,36 @@ import urllib2
 from bs4 import BeautifulSoup
 
 
-def get_page_num():
-    """
-    获取透明售房网全杭州新房的价格页数
-    :return: int类型
-    """
-    url = "http://www.tmsf.com/newhouse/property_searchall.htm"
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'}
-    req = urllib2.Request(url, headers=headers)
-    page = urllib2.urlopen(req)
-    soup = BeautifulSoup(page, "html.parser")
-    title = soup.find_all("div", class_='pagenuber_info')
-    page_num_temp = title[0].contents[3].string
-    page_num = int(page_num_temp.split("/")[1])
-    return page_num
-
 def get_village_href(url):
     """
     :param url:输入url，随后获取这个页面的所有一房一价页面的url信息
     :return:list url信息
     """
-    village_id_list=[]
+    village_id_list = []
     headers = {'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'}
     req = urllib2.Request(url, headers=headers)
     page = urllib2.urlopen(req)
     soup = BeautifulSoup(page, "html.parser")
     for title in soup.find_all('div', class_="build_txt2"):
-        village_id_list.append("http://www.tmsf.com"+str(title.contents[1]["href"]).replace("dynamic","price"))
+        village_id_list.append("http://www.tmsf.com" + str(title.contents[1]["href"]).replace("dynamic", "price"))
     return village_id_list
+
+
+def get_village_page_num(url):
+    """
+    获取本小区的房子数量的页面总数
+    :param url:
+    :return:int类型
+    """
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'}
+    req = urllib2.Request(url, headers=headers)
+    page = urllib2.urlopen(req)
+    soup = BeautifulSoup(page, "html.parser")
+    title = soup.find_all("div", class_='spagenext')
+    return int(title[0].contents[1].string.split("/")[1].split()[0])
+
 
 if __name__ == '__main__':
     # get_page_num()
-    print get_village_href("http://www.tmsf.com/newhouse/property_searchall.htm?searchkeyword=&keyword=&sid=&districtid=&areaid=&dealprice=&propertystate=&propertytype=&ordertype=&priceorder=&openorder=&view720data=&page=1&bbs=&avanumorder=&comnumorder=")
+    print get_village_page_num(
+        "http://www.tmsf.com/newhouse/property_33_511129767_price.htm?isopen=&presellid=&buildingid=&area=&allprice=&housestate=&housetype=&page=1")
